01b56975ae41e4115af42c818293024c84784104
HrideyVerma79 <82920883+HrideyVerma79@users.noreply.github.com>
2023-09-17 17:42:03 +0530

    soldier class & left-right movement created

--- main.py
+++ main.py
@@ -1,18 +1,100 @@
-import pygame 
+import pygame
 
 pygame.init()
 
-SCREEN_WIDTH=600
+SCREEN_WIDTH=800
 SCREEN_HEIGHT=int(SCREEN_WIDTH*0.8)
+
+moving_left=False
+moving_right=False
+
+#set framerate
+clock=pygame.time.Clock()
+FPS=60
 
 screen=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption("Oscar's Adventure")
 
+#define colors
+BG=(144,201,120)
+
+def draw_bg():
+   screen.fill(BG) 
+
+#for player and enemies
+class Soldier(pygame.sprite.Sprite):
+    def __init__(self,char_type,x,y,scale,speed):
+        pygame.sprite.Sprite.__init__(self)
+        self.char_type = char_type
+        self.speed=speed
+        self.direction = 1
+        self.flip = False
+        img=pygame.image.load(f'img/{self.char_type}/Idle/0.png')
+        self.image=pygame.transform.scale(img,(int(img.get_width()*scale),int(img.get_height()*scale)))
+        self.rect=self.image.get_rect()
+        self.rect.center=(x,y)
+        
+    def move(self,moving_left,moving_right):
+        dx=0
+        dy=0
+        
+        if moving_left:
+            dx-=self.speed
+            self.flip = True
+            self.direction = -1
+        
+        if moving_right:
+            dx+=self.speed
+            self.flip = False
+            self.direction = 1
+            
+        #update rect position
+        self.rect.x+=dx
+        self.rect.y+=dy
+         
+    
+    def draw(self):
+        screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
+        
+        
+#player
+player=Soldier('player',200,200,3,5)
+enemy=Soldier('enemy',200,200,3,5)
+
+#game loop
 run =True
 while run:
+    clock.tick(FPS)
+    
+    draw_bg()
+    
+    player.draw()
+    enemy.draw()
+    player.move(moving_left,moving_right)
+    
+    #event handlers
     for event in pygame.event.get():
         #quit game
         if event.type==pygame.QUIT:
             run=False
-            
+        
+        #keyboard press
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_a:
+                moving_left=True
+            if event.key==pygame.K_d:
+                moving_right=True
+            if event.key==pygame.K_ESCAPE:
+                run=False
+        
+        #keyboard button release
+        if event.type==pygame.KEYUP:
+            if event.key==pygame.K_a:
+                moving_left=False
+            if event.key==pygame.K_d:
+                moving_right=False
+                
+    
+    pygame.display.update()
+    
 pygame.quit()
